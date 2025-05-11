52d35164edcc6cb733b5eaa02caa6f62a200fd3d
Valerio Clemenzi <nc@vcz.me>
2025-05-11 20:21:23 +0200

    feat: add Wine installation check in app script

--- scripts/app.sh
+++ scripts/app.sh
@@ -5,6 +5,12 @@ export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
 wine_path=$(which wine)
 assets_path="$(dirname "$0")"
 
+if [ -z "$wine_path" ]; then
+  osascript -e 'display dialog "Wine is not installed. Please install it first." with title "Flowgorithm" buttons {"OK"} default button "OK"'
+  open https://www.winehq.org/
+  exit 1
+fi
+
 # run wine
 $wine_path $assets_path/Flowgorithm.exe
 
